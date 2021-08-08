6e702f05d1fd9bf2f1382e65fa8203689ada00f0
Paolo Pepito <npaolopepito@gmail.com>
2021-08-08 22:15:34 +0800

    Add empty SharedPreferenceServices class

---
+++ lib/data/shared_preferences_services.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesServices {
+  SharedPreferencesServices._();
+  static final instance = SharedPreferencesServices._();
+}
