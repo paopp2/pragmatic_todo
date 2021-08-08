79678afebb019d83aa1f611c609ccfb509be069c
Paolo Pepito <npaolopepito@gmail.com>
2021-08-09 02:12:19 +0800

    Remove unused files

--- lib/data/shared_preferences_services.dart
+++
@@ -1,6 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPreferencesServices {
-  SharedPreferencesServices._();
-  static final instance = SharedPreferencesServices._();
-}
