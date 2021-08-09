a0dcdc7f6a42d8f0c4c5c1a67e149e6c43397d28
Paolo Pepito <npaolopepito@gmail.com>
2021-08-09 12:27:57 +0800

    Convert RegisterScreen to HookConsumerWidget, add Form functionality

--- lib/ui/auth/register_screen.dart
+++ lib/ui/auth/register_screen.dart
@@ -1,47 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pragmatic_todo/logic/auth/auth_providers.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends HookConsumerWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.read(authControllerProvider);
+
+    useEffect(() {
+      controller.initState();
+      return controller.dispose;
+    }, []);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
           body: SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Username',
+              child: Form(
+                key: controller.registerFormKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: controller.tecUsername,
+                      validator: controller.registerUsernameValidator,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Username',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Password',
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: controller.tecPassword,
+                      validator: controller.registerPasswordValidator,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Confirm password',
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: controller.tecConfirmPass,
+                      validator: controller.registerPasswordValidator,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Confirm password',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Register"),
-                  )
-                ],
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: controller.register,
+                      child: const Text("Register"),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
