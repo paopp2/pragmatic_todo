f572ae25951339ad12bd13a1f01dd9e11a6e656b
Paolo Pepito <npaolopepito@gmail.com>
2021-08-09 15:08:13 +0800

    Add logOut button in HomeScreen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pragmatic_todo/logic/home/home_providers.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.read(homeControllerProvider);
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: controller.logOut,
+                icon: const Icon(Icons.logout_outlined),
+              )
+            ],
+          ),
           body: SafeArea(
             child: Column(
               children: [
