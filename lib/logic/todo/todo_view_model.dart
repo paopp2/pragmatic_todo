860b718f3829f7411bca359a6caa1d3d8e0e8413
Paolo Pepito <npaolopepito@gmail.com>
2021-08-11 00:37:59 +0800

    Convert TodoView to HookConsumerWidget, add unimplemented TodoViewModel

---
+++ lib/logic/todo/todo_view_model.dart
@@ -0,0 +1,10 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TodoViewModel {
+  TodoViewModel(this.read);
+  final Reader read;
+
+  void initState() {}
+  void saveTodo() {}
+  void dispose() {}
+}
--- lib/ui/todo/todo_view.dart
+++ lib/ui/todo/todo_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pragmatic_todo/logic/todo/todo_view_model.dart';
 
-class TodoView extends StatelessWidget {
+class TodoView extends HookConsumerWidget {
   const TodoView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final model = TodoViewModel(ref.read);
+
+    useEffect(() {
+      model.initState();
+      return model.dispose;
+    }, []);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
