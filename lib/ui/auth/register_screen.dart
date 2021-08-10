bf94d5cedf4fa06cccc693d8a0d65273c24be099
Paolo Pepito <npaolopepito@gmail.com>
2021-08-10 10:06:23 +0800

    Refactor AuthController to be an object instead of provider

--- lib/ui/auth/register_screen.dart
+++ lib/ui/auth/register_screen.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pragmatic_todo/app_router.dart';
-import 'package:pragmatic_todo/logic/auth/auth_providers.dart';
+import 'package:pragmatic_todo/logic/auth/auth_controller.dart';
 
 class RegisterScreen extends HookConsumerWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.read(authControllerProvider);
+    final controller = AuthController(ref.read);
 
     useEffect(() {
       controller.initState();
