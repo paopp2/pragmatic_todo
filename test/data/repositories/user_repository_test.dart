64fb4cfe7c75ed3fee5ae1e80548dea41a4372c2
Paolo Pepito <npaolopepito@gmail.com>
2021-08-13 01:00:24 +0800

    Update tests for UserRepository

--- test/data/repositories/user_repository_test.dart
+++ test/data/repositories/user_repository_test.dart
@@ -1,4 +1,3 @@
-import 'package:pragmatic_todo/data/helpers/shared_preferences_helper.dart';
 import 'package:pragmatic_todo/data/repositories/user_repository.dart';
 import 'package:pragmatic_todo/model/user/user.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -8,9 +7,9 @@ void main() {
   final _goodFakePrefsHelper = FakeSharedPreferencesHelper(isValid: true);
   final _badFakePrefsHelper = FakeSharedPreferencesHelper(isValid: false);
 
-  group('addNewUser', () {
+  group('addNewUser:', () {
     const User testUser = User(username: "u", password: "p");
-    group('Given SharedPreferencesHelper returns valid results, ', () {
+    group('Given SharedPreferencesHelper returns valid results,', () {
       final userRepository = UserRepository(_goodFakePrefsHelper);
       test('the methods returns normally (no Exception)', () {
         expect(userRepository.addNewUser(testUser), isNot(Exception));
@@ -20,7 +19,7 @@ void main() {
         expect(await userRepository.addNewUser(testUser), true);
       });
     });
-    group('Given SharedPreferencesHelper returns invalid results, ', () {
+    group('Given SharedPreferencesHelper returns invalid results,', () {
       final userRepository = UserRepository(_badFakePrefsHelper);
 
       test('the method returns normally (no Exception)', () {
@@ -33,21 +32,90 @@ void main() {
     });
   });
 
-  group('getAllUsers: ', () {
+  group('updateUser:', () {
+    const User testUser = User(username: "u", password: "p");
+    group('Given SharedPreferencesHelper returns valid results,', () {
+      final userRepository = UserRepository(_goodFakePrefsHelper);
+      test('the methods returns normally (no Exception)', () {
+        expect(userRepository.updateUser(testUser), isNot(Exception));
+      });
+
+      test('returns false if user is not found', () async {
+        expect(await userRepository.updateUser(testUser), false);
+      });
+
+      final existingUser = User.fromJson(goodTestJsonUserList[0]);
+
+      test('returns true if user is found and updated', () async {
+        expect(await userRepository.updateUser(existingUser), true);
+      });
+    });
+    group('Given SharedPreferencesHelper returns invalid results,', () {
+      final userRepository = UserRepository(_badFakePrefsHelper);
+
+      test('the method returns normally (no Exception)', () {
+        expect(userRepository.updateUser(testUser), isNot(Exception));
+      });
+
+      test('returns false', () async {
+        expect(await userRepository.updateUser(testUser), false);
+      });
+    });
+  });
+
+  group('getUser:', () {
+    group('Given SharedPreferencesHelper returns valid results,', () {
+      final userRepository = UserRepository(_goodFakePrefsHelper);
+      test('the methods returns normally', () {
+        expect(userRepository.getUser("someuser"), isNot(Exception));
+      });
+
+      test('returns correct data when user exists', () async {
+        final userA = User.fromJson(goodTestJsonUserList[0]);
+        final userB = User.fromJson(goodTestJsonUserList[1]);
+        expect(await userRepository.getUser("userA"), userA);
+        expect(await userRepository.getUser("userB"), userB);
+      });
+
+      test('returns null when user doesn\'t exist', () async {
+        expect(await userRepository.getUser("nonexistent"), null);
+      });
+    });
+
+    group('Given SharedPreferencesHelper returns invalid results,', () {
+      final userRepository = UserRepository(_badFakePrefsHelper);
+
+      test('the method returns normally (no Exception)', () {
+        expect(userRepository.getUser("nonexistent"), isNot(Exception));
+      });
+
+      test('returns null', () async {
+        expect(await userRepository.getUser("random"), null);
+      });
+    });
+  });
+
+  group('getAllUsers:', () {
     final userRepository = UserRepository(_goodFakePrefsHelper);
 
-    group('Given SharedPreferencesHelper returns valid results, ', () {
+    group('Given SharedPreferencesHelper returns valid results,', () {
       test('the method returns normally', () {
         expect(userRepository.getAllUsers, returnsNormally);
       });
 
-      test('correct data is returned', () async {
-        final userRepository = UserRepository(_goodFakePrefsHelper);
+      test('returned list has the expected length', () async {
+        final userList = await userRepository.getAllUsers();
+        expect(userList.length, goodTestJsonUserList.length);
+      });
+
+      test('returned list contains the expected data', () async {
         expect(
           await userRepository.getAllUsers(),
           isA<List<User>>()
-              .having((l) => l[0].username, "username", contains("paolo"))
-              .having((l) => l[0].password, "password", contains("paolo")),
+              .having((l) => l[0].username, "username", contains("userA"))
+              .having((l) => l[0].password, "password", contains("passA"))
+              .having((l) => l[1].username, "username", contains("userB"))
+              .having((l) => l[1].password, "password", contains("passB")),
         );
       });
     });
