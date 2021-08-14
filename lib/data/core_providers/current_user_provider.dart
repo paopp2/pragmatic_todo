bfb50d4c793f45ec94a4dd16e9482e33d653aae1
Paolo Pepito <npaolopepito@gmail.com>
2021-08-14 10:53:44 +0800

    Add documentation for CurrentUserProvider

--- lib/data/core_providers/current_user_provider.dart
+++ lib/data/core_providers/current_user_provider.dart
@@ -1,13 +1,19 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pragmatic_todo/model/user/user.dart';
 
+/// Exposes the currentUser to the app
 final currentUserProvider =
     StateNotifierProvider.autoDispose<CurrentUserNotifier, User?>(
         (ref) => CurrentUserNotifier(null));
 
+/// Controls the currentUserProvider
 class CurrentUserNotifier extends StateNotifier<User?> {
   CurrentUserNotifier(this.currentUser) : super(currentUser);
   final User? currentUser;
+
+  /// Set a new [user]
   void setNew(User user) => state = user;
+
+  /// Remove the currentUser
   void remove() => state = null;
 }
